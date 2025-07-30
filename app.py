72fbdd5a4b9b9888eee96646eae70dcdf5a02bcd
VINAY B V <106902815+vinay-B-V-37@users.noreply.github.com>
2025-07-30 22:22:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,377 @@
+from flask import Flask, render_template, url_for, request
+import os
+from werkzeug.utils import secure_filename
+import tensorflow as tf
+from keras.models import load_model
+import cv2
+from PIL import Image
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+
+# Heart Prediction
+@app.route("/heart")
+def heart():
+    return render_template("heart.html")
+
+
+def PredictorHD(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1, size)
+    if(size == 13):
+        loaded_model = joblib.load("Trained Model/Heart-Model/heart_model.pkl")
+        result = loaded_model.predict(to_predict)
+
+    return result[0]
+
+
+@app.route('/predictHD', methods=["POST"])
+def predictHD():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        input_data = {key: to_predict_list[key] for key in to_predict_list if key not in ['name', 'contact', 'patient_id']}
+        to_predict_list = list(input_data.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if len(to_predict_list) == 13:
+            result = PredictorHD(to_predict_list, 13)
+            # Gather input data and prediction result to create a report
+            report_data = {
+                "name": request.form["name"],
+                "contact": request.form["contact"],
+                "patient_id": request.form["patient_id"],
+                "sex": "Male" if request.form["sex"] == "1" else "Female",
+                "chest_pain_type": request.form["cp"],
+                "resting_blood_pressure": request.form["trestbps"],
+                "serum_cholesterol": request.form["chol"],
+                "fasting_blood_sugar": "Fasting Blood Sugar > 120 mg/dl" if request.form["fbs"] == "1" else "Fasting Blood Sugar < 120 mg/dl",
+                "rest_ecg": request.form["restecg"],
+                "max_heart_rate": request.form["thalach"],
+                "exercise_induced_angina": "Yes" if request.form["exang"] == "1" else "No",
+                "st_depression": request.form["oldpeak"],
+                "slope_of_peak_exercise_st_segment": request.form["slope"],
+                "num_major_vessels_colored_by_flourosopy": request.form["ca"],
+                "thalium_stress_result": request.form["thal"],                
+                "prediction_result": " Cardiopathic Nullity: Patient exhibits absence of heart disease" if int(result) == 1 else "Cardiopathic Nullity: Patient exhibits absence of heart disease"
+            }
+            return render_template("heart_result.html", report_data=report_data, input_data=input_data)
+
+
+
+# Breast Cancer prediction
+@app.route("/BreastCancer")
+def BreastCancer():
+    return render_template("cancer.html")
+
+
+def PredictorBC(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1, size)
+    if(size == 13):
+        loaded_model = joblib.load(
+            'Trained Model/breast-cancer/cancer_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/predictBC', methods=["POST"])
+def predictBC():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+        if(len(to_predict_list) == 13):
+            result = PredictorBC(to_predict_list, 13)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+
+# Diabetes
+
+
+@app.route("/Diabet")
+def diabet():
+    return render_template("diabetes.html")
+
+
+@app.route('/predictDiabet', methods=["POST"])
+def predictDiabet():
+    loaded_model = joblib.load('Trained Model/Diabet/diabetes_model.pkl')
+
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+        print(result)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+
+@app.route("/kidney")
+def kidney():
+    return render_template("kidney.html")
+
+
+@app.route('/predictKD', methods=["POST"])
+def predictKD():
+
+    loaded_model = joblib.load('Trained Model/kidney/kidney_model.pkl')
+
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+
+    if(int(result) == 1):
+        prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+# Liver disease
+
+
+@app.route("/liver")
+def liver():
+    return render_template("liver.html")
+
+
+@app.route('/predictLD', methods=["POST"])
+def predictLD():
+
+    loaded_model = joblib.load('Trained Model/liver/liver_model.pkl')
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+# brain tumor
+
+
+
+@app.route('/BrainTumor')
+def brain_tumor():
+    return render_template('brain_tumor.html')
+
+
+def predict_Btumor(img_path):
+    model_load = load_model("Trained Model/brain tumor/brain_tumor1.keras")
+
+    img = cv2.imread(img_path)
+    img = Image.fromarray(img)
+    img = img.resize((64, 64))
+    img = np.array(img)
+    img = np.expand_dims(img, axis=0)
+
+    preds = model_load.predict(img)
+    return preds[0]
+
+
+@app.route('/predictBTumor', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+
+        preds = predict_Btumor(file_path)
+        # print(preds)
+
+        if int(preds[0]) == 0:
+            result = "No worry! No Brain Tumor"
+        else:
+            result = "Patient has Brain Tumor"
+
+        # print(f'prdicted: {result}')
+
+        return result
+
+    return None
+
+
+# Stroke Disease
+@app.route("/stroke")
+def stroke():
+    return render_template("stroke.html")
+
+
+@app.route('/predictStroke', methods=["POST"])
+def predictStroke():
+
+    loaded_model = joblib.load('Trained Model/stroke/stroke_model.pkl')
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        # print(to_predict_list)
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+
+# Hepatitis
+@app.route("/hepatitis")
+def hepatitis():
+    return render_template("hepatitis.html")
+
+
+@app.route('/predictHepatitis', methods=["POST"])
+def predictHepatitis():
+
+    loaded_model = joblib.load('Trained Model/hepatitis/hepatitis_model.pkl')
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        # print(to_predict_list)
+        to_predict_list = list(map(float, to_predict_list))
+
+        to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
+        result = loaded_model.predict(to_predict)
+
+    if(int(result) == 1):
+        prediction = "Sorry! it seems getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))
+
+
+# ct scan images lung cancer detection
+def predict_label(img_path):
+	model = load_model("Trained Model/ct scan lung cancer/ct-scan-model1.keras")
+
+	img = cv2.imread(img_path)
+	img = Image.fromarray(img)
+	img = img.resize((224, 224))
+	img = np.array(img)
+	img = np.expand_dims(img, axis=0)
+
+	pred = model.predict(img)
+	return pred[0]
+
+@app.route("/lung")
+def lung():
+	return render_template("lung.html")
+
+@app.route("/preditLC", methods = ['GET', 'POST'])
+def get_output():
+	dic ={ 0:"Adenocarcinoma", 1:"Carcinoma", 2:"Normal", 3:"Squamous"}
+
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/ct scan images/" + img.filename
+		img.save(img_path)
+
+		p = np.argmax(predict_label(img_path))
+		# print(p)
+
+	return render_template("lung.html", prediction = dic[p], img_path = img_path)
+
+
+# Ocular disease
+def predict_label1(img_path):
+	model = load_model("Trained Model/ocular/model.h5")
+
+	img = cv2.imread(img_path)
+	img = Image.fromarray(img)
+	img = img.resize((224, 224))
+	img = np.array(img)
+	img = np.expand_dims(img, axis=0)
+
+	pred = model.predict(img)
+	return pred[0]
+
+
+@app.route("/ocular")
+def ocular():
+	return render_template("ocular.html")
+
+
+@app.route("/predictOcular", methods = ['GET', 'POST'])
+def predictOcular():
+	dic ={ 0:"No chance of disease", 1:"chance Of Ocular disease!"}
+
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/ocular images/" + img.filename
+		img.save(img_path)
+
+		p = predict_label1(img_path)
+		print(round(p[0]))
+
+	return render_template("ocular.html", prediction = dic[round(p[0])], img_path = img_path)
+
+
+# Skin cancer
+def predict_label2(img_path):
+	model = load_model("Trained Model\skin cancer\skin_cancer1.keras")
+
+	img = cv2.imread(img_path)
+	img = Image.fromarray(img)
+	img = img.resize((224, 224))
+	img = np.array(img)
+	img = np.expand_dims(img, axis=0)
+
+	pred = model.predict(img)
+	return pred[0]
+
+
+@app.route("/skin")
+def skin():
+	return render_template("skin.html")
+
+
+@app.route("/predictSkinC", methods = ['GET', 'POST'])
+def predictSkinC():
+	dic ={ 0:"Benign", 1:"Malignant!"}
+
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/skin images/" + img.filename
+		img.save(img_path)
+
+		p = predict_label2(img_path)[0]
+		print(np.round(p))
+
+	return render_template("skin.html", prediction = dic[np.round(p)], img_path = img_path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
